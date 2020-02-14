0e51c83105673b45a97a5d35afe434c2b48b4dde
Chris Castillo <46719081+chris-castillo-dev@users.noreply.github.com>
2020-02-14 15:25:39 -0500

    Create reset-function.php

---
+++ reset-function.php
@@ -0,0 +1,15 @@
+
+function resetPages() { 
+	global $wpdb;
+
+	$results = $wpdb->get_results("UPDATE `wp_posts` SET `post_content`='' WHERE ID=275");
+	$results = $wpdb->get_results("SELECT ID FROM `wp_posts` WHERE post_type='page'");
+
+	foreach( $results as $page ){
+		$sql = "DELETE FROM `wp_postmeta` WHERE `meta_key` LIKE '%elementor%' AND `post_id`=" . $page->ID;
+		$results2 = $wpdb->get_results($sql);
+	}
+
+}
+
+resetPages();
